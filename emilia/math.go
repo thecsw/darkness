2bfb32a67faacd219ce13db5cd13cd90194967b7
Sandy <ctu@ku.edu>
2022-03-14 17:20:19 -0500

    Use katex for math rendering, it's just better all around
    Signed-off-by: Sandy <ctu@ku.edu>

--- emilia/math.go
+++ emilia/math.go
@@ -6,7 +6,17 @@ import (
 
 const (
 	mathJax = `<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
-<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>`
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>`
+
+	katexJs = `
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.css" integrity="sha384-KiWOvVjnN8qwAZbuQyWDIbfCLFhLXNETzBQjA/92pIowpC0d2O3nppDGQVgwd2nB" crossorigin="anonymous">
+    <!-- The loading of KaTeX is deferred to speed up page rendering -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/katex.min.js" integrity="sha384-0fdwu/T/EQMsQlrHCCHoH10pkPLlKA1jL5dFyUOvB3lfeT2540/2g6YgSi2BL14p" crossorigin="anonymous"></script>
+    <!-- To automatically render math in text elements, include the auto-render extension: -->
+    <script defer src="https://cdn.jsdelivr.net/npm/katex@0.15.3/dist/contrib/auto-render.min.js" integrity="sha384-+XBljXPPiv+OzfbB3cVmLHf4hdUFHlWNZN5spNQ7rmHTXpd7WvJum6fIACpNNfIR" crossorigin="anonymous"
+        onload="renderMathInElement(document.body);"></script>
+`
+	mathJs = katexJs
 )
 
 func AddMathSupport(page *internals.Page) {
@@ -16,7 +26,7 @@ func AddMathSupport(page *internals.Page) {
 		if content.IsParagraph() {
 			// If found, add the script and leave
 			if internals.MathRegexp.MatchString(content.Paragraph) {
-				page.Scripts = append(page.Scripts, mathJax)
+				page.Scripts = append(page.Scripts, mathJs)
 				return
 			}
 		}
@@ -24,7 +34,7 @@ func AddMathSupport(page *internals.Page) {
 		if content.IsHeading() {
 			// If found, add the script and leave
 			if internals.MathRegexp.MatchString(content.Heading) {
-				page.Scripts = append(page.Scripts, mathJax)
+				page.Scripts = append(page.Scripts, mathJs)
 				return
 			}
 		}
@@ -33,7 +43,7 @@ func AddMathSupport(page *internals.Page) {
 			for _, item := range content.List {
 				// If found, add the script and leave
 				if internals.MathRegexp.MatchString(item) {
-					page.Scripts = append(page.Scripts, mathJax)
+					page.Scripts = append(page.Scripts, mathJs)
 					return
 				}
 
