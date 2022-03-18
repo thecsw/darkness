3341a13400508eb3e0385e5cae0e12c7dd0e0b99
Sandy <ctu@ku.edu>
2022-03-18 03:27:28 -0500

    In source code left trimming, ignore empty lines
    Signed-off-by: Sandy <ctu@ku.edu>

--- emilia/source.go
+++ emilia/source.go
@@ -18,6 +18,10 @@ func SourceCodeTrimLeftWhitespace(page *internals.Page) {
 		}
 		offset := len(lines[0]) - len(strings.TrimLeft(lines[0], " "))
 		for i, line := range lines {
+			// It's just a whitespace line, then ignore it
+			if len(strings.TrimSpace(line)) < 1 {
+				continue
+			}
 			// if the initial offset is bigger, then abort the whole thing
 			if len(line)-len(strings.TrimLeft(line, " ")) < offset {
 				return
