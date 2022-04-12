d8c649963453ba8ee95508472e0fa3af9dfe5aec
Sandy <sandy@sandyuraz.com>
2022-04-11 19:03:21 -0500

    Allow `“”` to be included in markup delimeters
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- internals/regex.go
+++ internals/regex.go
@@ -8,15 +8,15 @@ var (
 	// URLRegexp is yoinked from https://ihateregex.io/expr/url/
 	URLRegexp = regexp.MustCompile(`(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*))`)
 	// BoldText is the regexp for matching bold text
-	BoldText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])\*(\S|\S\S|\S.+\S)\*($|[ ()\[\],.!?;&_%<>])`)
+	BoldText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])\*(\S|\S\S|\S.+\S)\*($|[ ()\[\],.!?;&_%<>“”])`)
 	// ItalicText is the regexp for matching italic text
-	ItalicText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])/(\S|\S\S|\S.+\S)/($|[ ()\[\],.!?;&_%<>])`)
+	ItalicText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])/(\S|\S\S|\S.+\S)/($|[ ()\[\],.!?;&_%<>“”])`)
 	// BoldItalicText is the regexp for matching bold-italic text from the left
 	BoldItalicTextBegin = regexp.MustCompile(`(?mU)(^|[ ()_%<>])\*/`)
 	// BoldItalicTextEnd is the regexp for matching bold-italic text from the right
 	BoldItalicTextEnd = regexp.MustCompile(`(?mU)/\*($|[ (),.!?;&_%<>])`)
 	// VerbatimText is the regexp for matching verbatim text
-	VerbatimText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])=(\S|\S\S|\S.+\S)=($|[ ()\[\],.!?;&_%<>])`)
+	VerbatimText = regexp.MustCompile(`(?mU)(^|[ ()\[\]_%<>])=(\S|\S\S|\S.+\S)=($|[ ()\[\],.!?;&_%<>“”])`)
 	// KeyboardRegexp is the regexp for matching keyboard text
 	KeyboardRegexp = regexp.MustCompile(`kbd:\[([^][]+)\]`)
 	// MathRegexp is the regexp for matching math text
