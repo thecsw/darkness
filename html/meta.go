2a4bfc507ca08fe935f3614d4e420db672900a76
Sandy <sandy@sandyuraz.com>
2022-05-25 16:02:24 -0500

    Remove all the orgmode formatting from meta titles, like descriptions
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- html/meta.go
+++ html/meta.go
@@ -73,7 +73,7 @@ func addBasic(page *internals.Page, description string) string {
 // addOpenGraph adds the opengraph preview meta tags
 func addOpenGraph(page *internals.Page, description string) string {
 	toAdd := []meta{
-		{"og:title", "og:title", html.EscapeString(page.Title)},
+		{"og:title", "og:title", html.EscapeString(processDescription(page.Title))},
 		{"og:site_name", "og:site_name", html.EscapeString(emilia.Config.Title)},
 		{"og:url", "og:url", page.URL},
 		{"og:locale", "og:locale", emilia.Config.Website.Locale},
@@ -99,7 +99,7 @@ func addTwitterMeta(page *internals.Page, description string) string {
 		{"twitter:creator", "twitter:creator", emilia.Config.Website.Twitter},
 		{"twitter:image:src", "twitter:image:src", strings.TrimRight(page.URL, "/") + "/" + emilia.Config.Website.Preview},
 		{"twitter:url", "twitter:url", page.URL},
-		{"twitter:title", "twitter:title", html.EscapeString(page.Title)},
+		{"twitter:title", "twitter:title", html.EscapeString(processDescription(page.Title))},
 		{"twitter:description", "twitter:description", html.EscapeString(description)},
 	}
 	twitterTags := make([]string, 7)
