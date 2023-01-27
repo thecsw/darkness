ff02c6b29b89be27cfd00b6b5854bebfaaf8b5ca
Sandy <sandy@sandyuraz.com>
2023-01-26 23:20:28 -0600

    Change orgmode's horizontal line to `-----` from `---`
    Signed-off-by: Sandy <sandy@sandyuraz.com>

--- parse/orgmode/defs.go
+++ parse/orgmode/defs.go
@@ -24,7 +24,7 @@ const (
 	optionDate         = "date:"
 	optionHtmlHead     = "html_head:"
 	optionOptions      = "options:"
-	horizontalLine     = "---"
+	horizontalLine     = "-----"
 
 	sectionLevelOne   = "* "
 	sectionLevelTwo   = "** "
