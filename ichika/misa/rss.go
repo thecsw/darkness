9faa97719be71e6d36a863ebd3a092acaf140678
Sandy <sandy@sandyuraz.com>
2025-10-16 02:24:47 -0500

    fixed misa rss with nested macros

--- ichika/misa/rss.go
+++ ichika/misa/rss.go
@@ -25,6 +25,7 @@ const (
 // GenerateRssFeed generates an RSS feed based on the given config and directories.
 func GenerateRssFeed(conf *alpha.DarknessConfig, rssFilename string, rssDirectories []string, dryRun bool) {
 	initLog()
+	recordGlobalMacros(conf)
 	// Get all all the pages we can build out.
 	allPages := hizuru.BuildPagesSimple(conf, rssDirectories)
 	// Try to retrieve the top root page to get channel description. If not found, use the
