4557cb52d99816bcdfa9c98577ccb2354a996f3c
Sandy <sandy@sandyuraz.com>
2025-10-16 02:20:15 -0500

    Support nested macros in misa.

--- ichika/misa/galleries.go
+++ ichika/misa/galleries.go
@@ -17,6 +17,7 @@ import (
 	"github.com/thecsw/darkness/v3/emilia/reze"
 	"github.com/thecsw/darkness/v3/ichika/hizuru"
 	"github.com/thecsw/darkness/v3/ichika/kuroko"
+	"github.com/thecsw/darkness/v3/parse/orgmode"
 	"github.com/thecsw/darkness/v3/yunyun"
 	"github.com/thecsw/gana"
 	"github.com/thecsw/rei"
@@ -40,7 +41,8 @@ func BuildGalleryFiles(conf *alpha.DarknessConfig, dryRun bool) {
 		puck.Logger.Fatalf("creating preview directory %s: %v", previewDirectory, err)
 	}
 
-	// Get all the gallery files.
+	// Get all the gallery files, some may need nested macros.
+	recordGlobalMacros(conf)
 	galleryFiles := getGalleryFiles(conf)
 
 	// Filter out all the files that already exist.
@@ -144,4 +146,27 @@ func getGalleryFiles(conf *alpha.DarknessConfig) []rem.GalleryItem {
 		}
 	}
 	return galleryFiles
+}
+
+// TODO: Figure out what to do with this, since it's also in ichika/build.go
+// recordGlobalMacros will read the global macros and inject them into pages.
+func recordGlobalMacros(conf *alpha.DarknessConfig) {
+	// Recall that this is primarily an orgmode feature, so we will lock it to that input ext.
+	if conf.Project.Input != puck.ExtensionOrgmode {
+		return
+	}
+	globalMacrosFile := yunyun.RelativePathFile("_macros" + conf.Project.Input)
+	globalMacrosFileFull := string(conf.Runtime.WorkDir.Join(globalMacrosFile))
+	if exists, err := rei.FileExists(globalMacrosFileFull); exists {
+		file, err := os.ReadFile(filepath.Clean(globalMacrosFileFull))
+		if err != nil {
+			conf.Runtime.Logger.Warn("Failed reading global macros file", "file", globalMacrosFileFull, "err", err)
+		}
+		if orgmode.CollectGlobalMacros(conf, globalMacrosFile, string(file)) {
+			conf.Runtime.Logger.Info("Loaded global macros")
+		}
+
+	} else if err != nil {
+		conf.Runtime.Logger.Warn("Failed checking global macros file", "err", err)
+	}
 }
